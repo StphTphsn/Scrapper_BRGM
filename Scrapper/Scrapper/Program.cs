78083e62f494e7e2daa88d0c188c6aa04cd99bef
Stéphane Deny <sdeny@free.fr>
2016-12-21 22:49:26 +0100

    crawlPage+proxy

--- Scrapper/Scrapper/Program.cs
+++ Scrapper/Scrapper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -8,34 +9,46 @@ using System.Threading.Tasks;
 
 namespace Scrapper
 {
-    class Morning
+    class Program
     {
         public static void Main()
         {
-            // Create a request for the URL. 		
-            WebRequest request = WebRequest.Create("https://discussions.apple.com/thread/3511875");
-            // If required by the server, set the credentials.
-            request.Credentials = CredentialCache.DefaultCredentials;
-            // Get the response.
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            // Display the status.
-            Console.WriteLine(response.StatusDescription);
-            // Get the stream containing content returned by the server.
-            Stream dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd();
-            // Display the content.
-            using (StreamWriter outfile = new StreamWriter("AllTxtFiles.txt"))
+            string reference = "BSS000DHKH";
+            string proxyIP = "212.20.63.36:8080";
+            crawlPage(reference, proxyIP);
+        }
+        public static void crawlPage(string reference, string proxyIP)
+        {
+            // Create a request for the URL.
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://ficheinfoterre.brgm.fr/InfoterreFiche/ficheBss.action?id=" + reference);
+            //request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+            request.Timeout = 60000;
+            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0";
+            request.KeepAlive = false;
+            request.ProtocolVersion = HttpVersion.Version10;
+            WebProxy myProxy = new WebProxy();
+            myProxy.Address = new Uri("http://" + proxyIP);
+            myProxy.UseDefaultCredentials = true;
+            request.Proxy = myProxy;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
             {
-                outfile.Write(responseFromServer);
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    // Open the stream using a StreamReader for easy access.
+                    string responseFromServer;
+                    using (StreamReader reader = new StreamReader(dataStream))
+                    {
+                        responseFromServer = reader.ReadToEnd();
+                    }
+                    using (StreamWriter outfile = new StreamWriter(Shared.RAW_FILE_PATH))
+                    {
+                        outfile.Write(responseFromServer);//.Replace("\\\"", "\""));//
+                    }
+
+                }
             }
-            Console.WriteLine(responseFromServer);
-            // Cleanup the streams and the response.
-            reader.Close();
-            dataStream.Close();
-            response.Close();
         }
     }
 }
---
+++ Scrapper/Scrapper/Shared.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrapper
+{
+     static class Shared
+    {
+         public static string RAW_FILE_PATH = "../../../raw.txt";
+    }
+}
